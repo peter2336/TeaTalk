22d92e0a7571793c1232fb395d3aa10d8387b814
Peter Hsu <yuchehsu2336@gmail.com>
2023-12-22 17:22:13 +0800

    update default theme color : dark

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      {localStorage.setItem("chakra-ui-color-mode", "dark")}
       <ChakraProvider>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
