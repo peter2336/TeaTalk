d3f36a6bee8d2d7ec13a52a354adad22314e016b
Peter Hsu <yuchehsu2336@gmail.com>
2024-02-26 19:52:57 +0800

    Imported useEffect

--- client/src/component/authentication/Login.jsx
+++ client/src/component/authentication/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link as ReactRouterLink } from "react-router-dom";
 import { ChatState } from "../../context/ChatProvider";
