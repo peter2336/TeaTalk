9a2e582ca5344af626495a1b9cf9fbb8de4e78b4
Peter Hsu <yuchehsu2336@gmail.com>
2024-02-26 19:30:18 +0800

    Added homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,54 +1,324 @@
 import React, { useEffect, useState } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import Login from "../component/authentication/Login";
-import Singup from "../component/authentication/Singup";
 import {
-  Container,
   Box,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-  useDisclosure,
-  ScaleFade,
+  Grid,
+  GridItem,
+  Link as ChakraLink,
+  Image,
+  Text,
+  Link,
+  Flex,
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  AccordionIcon,
 } from "@chakra-ui/react";
+import SimpleBar from "simplebar-react";
+import { useInView } from "react-intersection-observer";
+import { useSpring, animated } from "@react-spring/web";
+
+const AnimatedBox = ({ children }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+  const props = useSpring({
+    opacity: inView ? 1 : 0,
+    transform: inView ? "translateY(0)" : "translateY(50px)",
+    config: { duration: 1500 },
+  });
+
+  return (
+    <animated.div ref={ref} style={props}>
+      {children}
+    </animated.div>
+  );
+};
 
 const HomePage = () => {
   const navigate = useNavigate();
-  const { onOpen } = useDisclosure();
   const [user, setUser] = useState();
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     setUser(user);
-
-    if (user) {
-      navigate("/chat");
-    }
-    if (!user) {
-      navigate("/login");
-    }
   }, [navigate]);
 
   return (
-    <Container maxW="md" centerContent justifyContent="center">
-      <Box bg="#313338" w="100%" p={4} borderRadius="lg">
-        <Tabs variant="unstyled">
-          <ScaleFade in={onOpen}>
-            <TabPanels>
-              <TabPanel>
-                <Login />
-              </TabPanel>
+    <SimpleBar style={{ maxHeight: "100%", width: "100%" }}>
+      <Grid
+        templateAreas={`"header" "main" "footer"`}
+        gridTemplateRows={"80px 2800px 200px"}
+      >
+        <GridItem
+          bg="#1D2022"
+          area={"header"}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <ChakraLink as={ReactRouterLink} to="/" pr={5}>
+            <Image
+              h="60px"
+              src="https://res.cloudinary.com/tea-talk/image/upload/v1708583548/teatalk_zjriap.png"
+            />
+          </ChakraLink>
+          <ChakraLink
+            as={ReactRouterLink}
+            to={user ? "/chat" : "/login"}
+            bg="#5865F2"
+            borderRadius="full"
+            padding="7px 16px"
+            transition="all .2s"
+            _hover={{
+              bg: "#7289DA",
+            }}
+          >
+            {user ? "開始聊天！" : "登入"}
+          </ChakraLink>
+        </GridItem>
 
-              <TabPanel>
-                <Singup />
-              </TabPanel>
-            </TabPanels>
-          </ScaleFade>
-        </Tabs>
-      </Box>
-    </Container>
+        <GridItem bg="#FFFFFF" area={"main"}>
+          <Box
+            bg="#1D2022"
+            h="700px"
+            color="#FFFFFF"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            px={10}
+          >
+            <AnimatedBox>
+              <Text fontSize="20px" textAlign="center">
+                在TeaTalk，您可以隨時隨地與三五好友一起排遣閒暇時光。這裡就是您輕鬆聊天的最佳去處。
+              </Text>
+              <Image
+                w={{ base: "600px", md: "800px" }}
+                src="https://res.cloudinary.com/tea-talk/image/upload/v1708662984/Conversation-pana_j14ebr.svg"
+              />
+            </AnimatedBox>
+          </Box>
+
+          <Box
+            bg="#F6F6F6"
+            h="700px"
+            color="#000000"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <AnimatedBox>
+              <Box
+                display={{ base: "block", md: "flex" }}
+                alignItems="center"
+                justifyContent="center"
+                px={10}
+              >
+                <Box textAlign="center" mr={{ md: "10" }}>
+                  <Image
+                    display="inline-block"
+                    boxShadow="2px 2px 2px 1px rgba(0, 0, 0, 0.2)"
+                    mb={{ base: 6, md: 0 }}
+                    alt="groupchat"
+                    w={{ base: "300px", md: "400px" }}
+                    borderRadius="md"
+                    src="https://res.cloudinary.com/tea-talk/image/upload/v1708666494/groupchat_jhs76l.png"
+                  />
+                </Box>
+
+                <Box>
+                  <Text
+                    fontSize={{ base: "24px", md: "32px", lg: "40px" }}
+                    fontWeight="bold"
+                    textAlign="center"
+                  >
+                    建立與親朋好友的聊天群組！
+                  </Text>
+                  <Text
+                    color="#777777"
+                    fontSize="20px"
+                    textAlign="center"
+                    mt="24px"
+                  >
+                    您可以在這裡與他人一起分享，或是聊聊自己今天過得怎樣。
+                  </Text>
+                </Box>
+              </Box>
+            </AnimatedBox>
+          </Box>
+
+          <Box
+            bg="#FFFFFF"
+            h="700px"
+            color="#000000"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <AnimatedBox>
+              <Box
+                display={{ base: "block", md: "flex" }}
+                alignItems="center"
+                justifyContent="center"
+                px={10}
+              >
+                <Box>
+                  <Text
+                    fontSize={{ base: "24px", md: "32px", lg: "40px" }}
+                    fontWeight="bold"
+                    textAlign="center"
+                  >
+                    即時通訊～
+                  </Text>
+                  <Text
+                    color="#777777"
+                    fontSize="20px"
+                    textAlign="center"
+                    mt="24px"
+                  >
+                    您發送的訊息可以零時差傳遞給對方彷彿彼此身處同一空間。
+                  </Text>
+                </Box>
+
+                <Box textAlign="center" ml={{ md: "10" }}>
+                  <Image
+                    display="inline-block"
+                    boxShadow="2px 2px 2px 1px rgba(0, 0, 0, 0.2)"
+                    mt={{ base: 6, md: 0 }}
+                    alt="groupchat"
+                    w={{ base: "500px", md: "600px" }}
+                    borderRadius="md"
+                    src="https://res.cloudinary.com/tea-talk/image/upload/v1708671993/realtimechat_qqcoru.gif"
+                  />
+                </Box>
+              </Box>
+            </AnimatedBox>
+          </Box>
+
+          <Box
+            bg="#F6F6F6"
+            h="700px"
+            color="#000000"
+            display="block"
+            px="15%"
+            py={20}
+          >
+            <Text
+              fontSize={{ base: "24px", md: "32px", lg: "40px" }}
+              fontWeight="bold"
+              textAlign="center"
+            >
+              常見問題？
+            </Text>
+            <Accordion allowToggle my={6} h="250px">
+              <AccordionItem>
+                <h2>
+                  <AccordionButton borderRadius="lg">
+                    <Box as="span" flex="1" textAlign="left" fontSize="20px">
+                      如何註冊會員？
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel fontSize="18px" color="#06C755">
+                  <Link onClick={() => navigate("/register")} cursor="pointer">
+                    這裡開始註冊！
+                  </Link>
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <AccordionButton borderRadius="lg">
+                  <Box as="span" flex="1" textAlign="left" fontSize="20px">
+                    如何建立群組聊天？
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel fontSize="18px" color="#06C755">
+                  點擊建立群組並設定群組資訊，即可開始聊天！
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <AccordionButton borderRadius="lg">
+                  <Box as="span" flex="1" textAlign="left" fontSize="20px">
+                    如何加入好友？
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel fontSize="18px" color="#06C755">
+                  點擊搜尋用戶並輸入好友電子信箱！
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+
+            <Text
+              my={5}
+              fontSize={{ base: "24px", md: "32px" }}
+              textAlign="center"
+            >
+              準備好開始您的旅程了嗎？
+            </Text>
+
+            <Flex justifyContent="center">
+              <Box
+                cursor="pointer"
+                onClick={() => navigate("/login")}
+                bg="#5865F2"
+                color="#FFFFFF"
+                borderRadius="full"
+                fontSize="20px"
+                padding="14px 32px"
+                transition="all .2s"
+                _hover={{
+                  bg: "#7289DA",
+                }}
+              >
+                開始聊天！
+              </Box>
+            </Flex>
+          </Box>
+        </GridItem>
+
+        <GridItem
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          bg="#23272A"
+          area={"footer"}
+          flexDirection="column"
+        >
+          <Text>Peter Hsu © 2024</Text>
+          <Flex my={5}>
+            <Link href="https://github.com/peter2336" mr={5}>
+              <Image
+                h="40px"
+                src="https://res.cloudinary.com/tea-talk/image/upload/v1708584713/github_upjqlg.png"
+                opacity="0.7"
+                _hover={{ opacity: 1 }}
+                transition="all .2s"
+              />
+            </Link>
+            <Link href="https://www.linkedin.com/in/peter-hsu-859a43276/">
+              <Image
+                h="40px"
+                src="https://res.cloudinary.com/tea-talk/image/upload/v1708584713/linkedin_dhiaf1.png"
+                opacity="0.7"
+                _hover={{ opacity: 1 }}
+                transition="all .2s"
+              />
+            </Link>
+          </Flex>
+          <Text>
+            Web illustrations by
+            <Link href="https://storyset.com/"> Storyset</Link>
+          </Text>
+        </GridItem>
+      </Grid>
+    </SimpleBar>
   );
 };
 
